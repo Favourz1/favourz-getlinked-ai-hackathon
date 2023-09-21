ef5182a84356066891f0db2b974d885b11fc4f3f
Favour Okoh <okohfavour91@gmail.com>
2023-09-21 16:37:24 +0100

    Chore: Initial project commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import './App.css'
+import Home from './pages/Home';
+import Timeline from './pages/Timeline';
+import Overview from './pages/Overview';
+import Contact from './pages/Contact';
+import Faq from './pages/Faq';
+import Register from "./pages/Register";
+
+function App() {
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/*" element={<div>Not Found</div>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
