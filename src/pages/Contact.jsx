0534109cc6a20369a2db871d713836a2dbefe919
Favour Okoh <okohfavour91@gmail.com>
2023-09-23 22:36:50 +0100

    Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,54 @@
+import { useState } from "react"
 import Layout from "./layouts/Layout"
 import instagram from "../assets/icons/instagram.svg"
 import twitter from "../assets/icons/twitter.svg"
 import facebook from "../assets/icons/facebook.svg"
 import linkedin from "../assets/icons/linkedin.svg"
 import purpleFlareImg from "../assets/images/purple-flare.png"
+import gradientStar from "../assets/icons/star-gradient.png"
+import axios from "axios"
+import {useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+ import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const schema = yup.object().shape({
+  first_name: yup.string().trim().required("First Name is required"),
+  email: yup.string().email().trim().required(),
+  phone_number: yup.string().min(10, 'Phone number must be exactly 10 digits'),
+  message: yup.string().trim().required(),
+})
+
+
 
 
 
 const Contact = () => {
+  const [isLoading, setIsLoading] = useState(false);
+    const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: '',
+    resolver: yupResolver(schema)
+  })
+
+async function submitContactForm(data){
+  // console.log("data", data)
+ const BASE_URL = " https://backend.getlinked.ai/hackathon/contact-form"
+  try {
+        setIsLoading(true)
+        const response = await axios.post(BASE_URL, data);
+        console.log(response.data);
+        toast("Thank You we have received your message.");
+      } catch (err) {
+        console.log(err);
+        toast("Error sending message.");
+      }
+      setIsLoading(false)
+}
   return (
     <Layout showFooter={false}>
         <section className="relative py-5 overflow-hidden">
@@ -38,6 +79,7 @@ const Contact = () => {
                     <img src={linkedin} alt="" />
                 </div>
               </div>
+              <img className="h-4 w-4 absolute top-[-100px] left-[70%]" src={gradientStar} alt="" />
             </div>
             <div className="relative flex flex-col justify-center w-full md:w-[70%] pt-10 pb-5 text-left">
               <div className="p-10 rounded-[12px] bg-[#ffffff08] shadow-contactForm">
@@ -47,29 +89,44 @@ const Contact = () => {
                 <div className="text-xl text-primaryPink font-bold font-clashDisplay mb-5">
                   Let us know  about it!
                 </div>
-                <form className="space-y-4 flex flex-col">
+                <form className="space-y-4 flex flex-col" onSubmit={handleSubmit(submitContactForm)}>
                   <div>
-                    <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="text" name="" placeholder="First Name" />
-                     <span className="text-red-500 text-sm"></span>
+                    <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="text" name="" placeholder="First Name" {...register('first_name')}/>
+                    { errors.first_name && <span className="text-red-500 text-sm">{errors.first_name.message}</span>}
                   </div>
                   <div>
-                    <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="email" name="" placeholder="Email" />
-                     <span className="text-red-500 text-sm"></span>
+                    <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="email" name="" placeholder="Email" {...register('email')} />
+                     {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
                   </div>
                   <div>
-                    <textarea placeholder="Message" className="resize-none w-full h-48 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" rows="10" cols="">
+                    <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="text" name="" placeholder="Phone Number" {...register('phone_number')} />
+                     {errors.phone_number && <span className="text-red-500 text-sm">{errors.phone_number.message}</span>}
+                  </div>
+                  <div>
+                    <textarea placeholder="Message" className="resize-none w-full h-48 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" rows="10" cols="" {...register('message')}>
 
                     </textarea>
-                    {/* <input className="w-full h-48 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="" name="" placeholder="Message" /> */}
-                     <span className="text-red-500 text-sm"></span>
+                     {errors.message && <span className="text-red-500 text-sm">{errors.message.message}</span>}
                   </div>
-                  <button className='px-5 py-2 w-[40%] self-center bg-btnBgGrad rounded-[4px] hover:scale-[0.98] hover:opacity-80' type="submit">Submit</button>
+                  <button className='px-5 py-2 w-[40%] self-center bg-btnBgGrad rounded-[4px] hover:scale-[0.98] hover:opacity-80 mr-3' type="submit">{isLoading &&<div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status"></div>}Submit</button>
                 </form>
               </div>
             </div>
           </div>
         </div>
       </section>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        />
     </Layout>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -16,54 +16,67 @@ const Register = () => {
               <img src={manWithDesk} alt="" />
             </div>
             <div className="relative flex flex-col justify-center w-full md:w-[60%] pt-10 pb-5 text-left">
-              <div className="p-10 rounded-[12px] bg-[#ffffff08] shadow-contactForm">
+              <div className="p-8 rounded-[12px] bg-[#ffffff08] shadow-contactForm">
                 <div className="text-3xl text-primaryPink font-bold font-clashDisplay leading-[2] ">
                   Register
                 </div>
                 <div className="text-md flex items-baseline font-medium mb-5">
                   Be part of this movement! <img src={peopleWalking} alt="" />
                 </div>
-                <div className="text-2xl flex items-baseline mb-5 uppercase">
+                <div className="text-xl flex items-baseline mb-5 uppercase">
                   Create your account
                 </div>
                 <form className="space-y-4 flex flex-col">
                  <div className="flex justify-center items-center w-full space-x-4 mb-4">
                    <div className="w-1/2 flex flex-col">
                     <label className="text-sm font-medium mb-3">Team Name</label>
-                     <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="text" name="" placeholder="First Name" />
+                     <input className="w-full  outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="text" name="" placeholder="Enter the name of your group" />
                       <span className="text-red-500 text-sm"></span>
                    </div>
                    <div className="w-1/2 flex flex-col">
                     <label className="text-sm font-medium mb-3">Phone</label>
-                     <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="email" name="" placeholder="Email" />
+                     <input className="w-full  outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="email" name="" placeholder="Enter your phone number" />
                       <span className="text-red-500 text-sm"></span>
                    </div>
                  </div>
                  <div className="flex justify-center items-center w-full space-x-4 mb-4">
                    <div className="w-1/2 flex flex-col">
-                    <label className="text-sm font-medium mb-3">Team Name</label>
-                     <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="text" name="" placeholder="First Name" />
+                    <label className="text-sm font-medium mb-3">Email</label>
+                     <input className="w-full  outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="text" name="" placeholder="Enter your email address" />
                       <span className="text-red-500 text-sm"></span>
                    </div>
                    <div className="w-1/2 flex flex-col">
-                    <label className="text-sm font-medium mb-3">Phone</label>
-                     <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="email" name="" placeholder="Email" />
+                    <label className="text-sm font-medium mb-3">Project Topic</label>
+                     <input className="w-full  outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="email" name="" placeholder="What is your group project topic" />
                       <span className="text-red-500 text-sm"></span>
                    </div>
                  </div>
                  <div className="flex justify-center items-center w-full space-x-4 mb-4">
                    <div className="w-1/2 flex flex-col">
-                    <label className="text-sm font-medium mb-3">Team Name</label>
-                     <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="text" name="" placeholder="First Name" />
+                    <label className="text-sm font-medium mb-3">Category</label>
+                    <select className="w-full  outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3">
+                      <option value="" disabled selected>Select your category</option>
+                      <option value="">Admin</option>
+                      <option value="">Admin</option>
+                    </select>
                       <span className="text-red-500 text-sm"></span>
                    </div>
                    <div className="w-1/2 flex flex-col">
-                    <label className="text-sm font-medium mb-3">Phone</label>
-                     <input className="w-full h-14 outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3" type="email" name="" placeholder="Email" />
+                    <label className="text-sm font-medium mb-3">Group Size</label>
+                     <select className="w-full  outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input px-6 py-3">
+                      <option value="" disabled selected>Select</option>
+                      <option value="">Admin</option>
+                      <option value="">Admin</option>
+                    </select>
                       <span className="text-red-500 text-sm"></span>
                    </div>
                  </div>
-                  <button className='px-5 py-2 w-[40%] self-center bg-btnBgGrad rounded-[4px] hover:scale-[0.98] hover:opacity-80' type="submit">Submit</button>
+                 <p className="text-xs text-primaryPink">Please review your registration details before submitting</p>
+                 <div className="flex space-x-2">
+                  <input className="outline-none bg-[#ffffff08] rounded border border-gray-700 focus:border-white shadow-input" type="checkbox" name="" value="" />
+                  <p className="text-xs font-medium">I agree with the event terms and conditions  and privacy policy</p>
+                 </div>
+                  <button className='px-5 py-2 w-full self-center bg-btnBgGrad rounded-[4px] hover:scale-[0.98] hover:opacity-80' type="submit">Register Now</button>
                 </form>
               </div>
             </div>
